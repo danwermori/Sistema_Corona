04dce5dbbdcbefcff465c1e7a9998d70adfb1358
Danwer Mori <165702841+danwermori@users.noreply.github.com>
2025-10-01 14:53:42 -0500

    Sistema Corona

---
+++ CoronaCircular.py
@@ -0,0 +1,6 @@
+print("Corona Circular")
+radio_ma = float(inpu("Ingrese radio mayor: "))
+radio_me = float(inpu("Ingrese radio menor: "))
+area = 3.14*(radio_ma**2 - radio_me**2)
+print("resultados")
+print("Area: ",area)
